1e79783947f1b06e35c844eca7b817517e06f847
TMC\mehrnoosh.rigi <mehrnoosh.rigi@tmc.nl>
2022-10-19 15:36:13 +0200

    fix termination ofvgame

--- src/bullscows/Main.java
+++ src/bullscows/Main.java
@@ -1,6 +1,5 @@
 package bullscows;
 
-import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,32 +8,35 @@ public class Main {
         Random rand = new Random();
         int randomNumber = rand.nextInt(10000);
 
-        int cows = 0;
-        int bulls = 0;
+
 
         String player_1;
         player_1 = Integer.toString(randomNumber);
 
-        System.out.println(
-                "The random number is generated: " + randomNumber);
+        System.out.println("The secret code is prepared: ****. ");
 
         System.out.println("Guess the 4 digits number: ");
 
         Scanner input = new Scanner(System.in);
-
-        String player_2 = input.next();
-        for(int i = 0; i < 4; i++) {
-            char digit = player_2.charAt(i);
-            if(player_1.charAt(i) == digit) {
-                bulls++;
-            }else if (player_1.indexOf(digit) > -1){
-                cows++;
+        boolean found = false;
+        while (!found){
+            String player_2 = input.next();
+            int cows = 0;
+            int bulls = 0;
+            for(int i = 0; i < 4; i++) {
+                char digit = player_2.charAt(i);
+                if(player_1.charAt(i) == digit) {
+                    bulls++;
+                }else if (player_1.indexOf(digit) > -1){
+                    cows++;
+                }
             }
-        }
-        if(bulls == 4){
-            System.out.println("Congrats");
-        } else {
-            System.out.println("bulls: "+ bulls + " and cows: "  + cows);
+            if(bulls == 4){
+                found = true;
+                System.out.println("Congrats");
+            } else {
+                System.out.println("bulls: "+ bulls + " and cows: "  + cows);
+            }
         }
     }
 }
